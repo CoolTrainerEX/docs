1804b2e89065ba0cbd307b3f19c7409006270c21
CoolTrainerEX <CoolTrainerEX@gmail.com>
2026-01-13 14:48:16 +0800

    go

--- src/commands/go.rs
+++ src/commands/go.rs
@@ -1,12 +1,22 @@
-use std::process::Command;
+use std::{env, fs, process::Command};
 
 use anstream::println;
 use anstyle::{AnsiColor, Style};
+use anyhow::Context;
 use clap::Subcommand;
+use dialoguer::{Input, theme::ColorfulTheme};
+use indicatif::ProgressBar;
 use tracing::info;
 
-use crate::commands::{
-    Commands, Generator, go::gin::Gin, root::Root, upgrade::Upgrade, utils::execute_command,
+use crate::{
+    DOCS_DIR,
+    commands::{
+        Commands, Generator,
+        go::gin::Gin,
+        root::Root,
+        upgrade::Upgrade,
+        utils::{Deps, execute_command},
+    },
 };
 
 pub(super) mod gin;
@@ -31,7 +41,78 @@ pub(super) struct Go;
 
 impl Generator for Go {
     fn generate(&self, name: String) -> anyhow::Result<()> {
-        todo!()
+        let msg_style = Style::new().fg_color(Some(AnsiColor::Blue.into()));
+        let strong_style = Style::new().bold();
+        let bar = ProgressBar::new(3);
+
+        info!("Generating Go project.");
+
+        let current_dir = env::current_dir().context("Failed to get current directory")?;
+        let proj_dir = current_dir.join(&name);
+
+        println!(
+            "{msg_style}Generating Go project in {msg_style:#}{strong_style}{}{strong_style:#}{msg_style}.{msg_style:#}",
+            proj_dir.display()
+        );
+        info!("Running init command.");
+
+        fs::create_dir_all(&proj_dir).context("Failed to create directory.")?;
+        env::set_current_dir(&proj_dir).context("Failed to change working directory.")?;
+
+        info!(dir = proj_dir.to_str());
+
+        execute_command(Command::new("go").args([
+            "mod",
+            "init",
+            &format!(
+                    "github.com/{}/{}",
+                    Input::<String>::with_theme(&ColorfulTheme::default())
+                        .with_prompt("User name")
+                        .interact_text()
+                        .context("Input error.")?,
+                    &name
+                ),
+        ]))?;
+        bar.inc(1);
+
+        info!("Done.");
+        info!("Running init commands.");
+
+        execute_command(Command::new("docker").arg("init"))?;
+
+        info!("Done.");
+        info!("Installing dependencies");
+
+        let deps: Deps = serde_json::from_slice(
+            DOCS_DIR
+                .get_file(self.docs_path().join("deps").with_extension("json"))
+                .context(format!(
+                    "Cannot find {strong_style}deps.json{strong_style:#}."
+                ))?
+                .contents(),
+        )
+        .context(format!(
+            "Failed to parse {strong_style}deps.json{strong_style:#}."
+        ))?;
+
+        if let Some(deps) = deps.deps {
+            execute_command(Command::new("go").args(["get", "-u"]).args(deps))?;
+        }
+
+        if let Some(dev) = deps.dev {
+            for dep in dev {
+                execute_command(Command::new("go").args(["get", "-tool"]).arg(dep))?;
+            }
+        }
+
+        bar.inc(1);
+
+        info!("Done.");
+        info!("Done generating project.");
+
+        bar.finish_and_clear();
+
+        Ok(())
     }
 
     fn docs_path(&self) -> std::path::PathBuf {
