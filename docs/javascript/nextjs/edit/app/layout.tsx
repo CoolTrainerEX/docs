bd51f7757a8d589ec5a6d46a8d4da69e0ed51b92
CoolTrainerEX <CoolTrainerEX@gmail.com>
2026-01-13 17:23:31 +0800

    fix

---
+++ docs/javascript/nextjs/edit/app/layout.tsx
@@ -0,0 +1,24 @@
+import { ThemeProvider } from "next-themes";
+
+export default function RootLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <html lang="en">
+            <body
+                className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+            >
+                <ThemeProvider
+                    attribute="class"
+                    defaultTheme="system"
+                    enableSystem
+                    disableTransitionOnChange
+                >
+                    {children}
+                </ThemeProvider>
+            </body>
+        </html>
+    );
+}
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use std::path::{Path, PathBuf};
+use std::{
+    env,
+    path::{Path, PathBuf},
+};
 
 use anstyle::Style;
 use anyhow::{Context, Result};
@@ -95,6 +98,7 @@ fn main() -> Result<()> {
         )
         .context("Failed to open documentation.")
     } else if args.local {
+        env::set_current_dir(config_dir()).context("Failed to set working directory.")?;
         open::that(
             Path::new(&config.path.context(format!(
                 "Config missing {strong_style}path{strong_style:#} value"
