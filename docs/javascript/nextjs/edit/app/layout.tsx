871be281775c4ca25731fea63b71a3db439d83b1
CoolTrainerEX <CoolTrainerEX@gmail.com>
2026-01-27 11:27:33 +0800

    sessfgzsgzdsrfg

--- docs/javascript/nextjs/edit/app/layout.tsx
+++ docs/javascript/nextjs/edit/app/layout.tsx
@@ -1,24 +1,27 @@
 import { ThemeProvider } from "next-themes";
+import Providers from "./providers.tsx";
 
 export default function RootLayout({
-    children,
+  children,
 }: Readonly<{
-    children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-    return (
-        <html lang="en">
-            <body
-                className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-            >
-                <ThemeProvider
-                    attribute="class"
-                    defaultTheme="system"
-                    enableSystem
-                    disableTransitionOnChange
-                >
-                    {children}
-                </ThemeProvider>
-            </body>
-        </html>
-    );
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <Providers>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+        </Providers>
+      </body>
+    </html>
+  );
 }
