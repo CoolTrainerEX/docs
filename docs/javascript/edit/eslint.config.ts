10baa61346ddc8ebe99f35c80bba9a604378e38f
CoolTrainerEX <CoolTrainerEX@gmail.com>
2026-02-10 12:34:48 +0800

    ye

--- docs/javascript/edit/eslint.config.ts
+++ docs/javascript/edit/eslint.config.ts
@@ -1,6 +1,8 @@
 import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+import { jsdoc } from "eslint-plugin-jsdoc";
 
 export default defineConfig([
     // Any other config imports go at the top
-    eslintPluginPrettierRecommended
+    eslintPluginPrettierRecommended,
+    jsdoc({ config: "flat/recommended" })
 ]);
